5c6faf97623c38c16423631940c08e343b1f4fe6
Arnaud <arnaud.dauguen@orange.fr>
2021-01-21 09:45:26 +0100

    tpCombat skeleton

---
+++ Assets/Scripts/tpCombat/fighter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tpCombat.fighter
+{
+   public class fighter : MonoBehaviour
+    {
+        protected int hp = 20;
+        protected int attack = 10;
+
+        public bool takeDamage(int damage)
+        {
+            this.hp -= damage;
+            return hp < 0;
+        }
+        public int getAttackStrength()
+        {
+            return this.attack;
+        }
+    }
+}
---
+++ Assets/Scripts/tpCombat/historyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace tpCombat.historyController
+{
+    public class historyController
+    {
+        public void fight() // called by button
+        {
+            //p1 attack p2
+            strikeBack(false);
+        }
+        public void defend() // called by button
+        {
+            //p1 attack p2
+            strikeBack(true);
+        }
+
+        private void strikeBack(bool isPlayerDefending)
+        {
+            //p2 attack p1
+            //if defending, player take only 20% damage
+        }
+
+        public void printHistory(fighter attacker, int damageOutput)
+        {
+            //change text off text area
+        }
+    }
+}
